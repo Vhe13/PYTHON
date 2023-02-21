37407a9567646de3451ef9aeb017249cb80ed2b2
Vhe13 <123860134+Vhe13@users.noreply.github.com>
2023-02-21 02:22:48 -0300

    Create aula3.py

---
+++ CURSO_PYTHON/aula3.py
@@ -0,0 +1,16 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+String são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# Aspas Simples
+
+# Aspas Duplas 
+
+# Escape
+
+# r
