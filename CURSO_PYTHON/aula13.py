c8d392c34ed4408801e815e06925989f4456857d
Vhe13 <123860134+Vhe13@users.noreply.github.com>
2023-02-23 21:32:52 -0300

    aulas

---
+++ CURSO_PYTHON/aula13.py
@@ -0,0 +1,15 @@
+# ... Usado para deixar como pendente.
+nome = 'Luiz Otávio'
+altura  = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+linha_1= f'{nome} tem {altura:.2f} de altura pesa {peso} quilos e seu imc é {imc:.2f}'
+
+print(linha_1)
+
+print(nome, 'tem', altura, 'de altura', 'pesa', peso, 'quilos e seu imc é', imc)
+
+# Luiz Otávio tem 1.80 de altura, 
+# pesa 95  quilos e seu IMC é 
+# 29.320987654320987
