ebe3b4bbebc9547a74f44707242029fb81b17174
Vhe13 <123860134+Vhe13@users.noreply.github.com>
2023-02-23 22:46:40 -0300

    aulas novas

---
+++ CURSO_PYTHON/aula17.py
@@ -0,0 +1,32 @@
+# codicao = #False #ou #True
+
+# if codicao:
+#     print('Esse é o código do if')
+# else: 
+#     print('Esse é o else do primeiro if')
+
+# if 10 == 10:
+#     print('Outro IF')   
+
+ 
+# print('fora do if')
+
+condicao1 = False
+condicao2 = False
+condicao3 = False
+condicao4 = False
+
+if condicao1: 
+    print('codigo para para condição 1')
+    print('codigo para para condição 1')
+elif condicao2:
+    print('codigo para para condição 2')
+    print('codigo para para condição 2')
+elif condicao3:
+    print('codigo para para condição 3')
+    print('codigo para para condição 3')
+elif condicao4:
+    print('codigo para para condição 4')
+    print('codigo para para condição 4')
+else:
+    print('Nada')
