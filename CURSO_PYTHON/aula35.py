34b169097bf7f5b2ec92ffb67bc49d7c865c162a
Vhe13 <123860134+Vhe13@users.noreply.github.com>
2023-03-01 20:51:22 -0300

    Exec

--- CURSO_PYTHON/aula35.py
+++ CURSO_PYTHON/aula35.py
@@ -4,11 +4,10 @@ while (enquanto)
 Executa uma ação enquanto uma condição for vendadeira
 loop Infinito -> Quando o código não tem fim.
 """
-condicao = True
-while condicao:
-    nome = input('Qual o seu nome?')
-    print(f'Seu nome é {nome}')
-    
-    if nome == 'sair':
-        break
+
+contador = 0
+
+while contador < 10:
+    contador = contador + 1
+    print(contador)
 print('Acabou')
---
+++ CURSO_PYTHON/aula36.py
@@ -0,0 +1,14 @@
+"""
+Repetições
+while (enquanto)
+Executa uma ação enquanto uma condição for vendadeira
+loop Infinito -> Quando o código não tem fim.
+
+Operadores de atribuição 
+= += -+ *= /= //= **= %=
+
+"""
+
+contador = 0
+contador += 2
+print(contador)
